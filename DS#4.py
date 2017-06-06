59132447512b2917196631df9d444a4d925bd7b9
Lee chang hwan <leechi38@naver.com>
2017-06-06 23:51:22 +0900

    과제4

--- DS#4.py
+++ DS#4.py
@@ -1,3 +1,15 @@
+def atoi(s):
+    rtr, sign=0, 1
+    s = s.strip()
+    if s[0] in '+-':
+        sc, s=s[0], s[1:]
+        if sc=='-':
+            sign=-1
+
+    for c in s:
+        rtr=rtr*10 + ord(c) - ord('0')
+
+    return sign*rtr
 
 class Node:
     def __init__(self, newval):
@@ -10,15 +22,20 @@ class Node:
 class RBT:
     def __init__(self):
         self.root = Node(None)
+        self.total = 0
+        self.nb = 0
+        self.bh = 0
     def insert(self, tree, n):
         if self.root.val is None:
             self.root = n
             self.expand(n)
+            self.total += 1
         elif n.val < tree.val:
             if tree.left.val is None:
                 tree.left = n
                 n.parent = tree
                 self.expand(n)
+                self.total +=1
             else:
                 self.insert(tree.left, n)
         else:
@@ -26,10 +43,27 @@ class RBT:
                 tree.right = n
                 n.parent = tree
                 self.expand(n)
+                self.total += 1
             else:
                 self.insert(tree.right, n)
         n.color = 'red'
         self.fixup(tree, n)
+
+    def search(self, n, val):
+        if(n.val is None):
+            return Exception
+        if(n.val is val):
+            return n
+        elif(n.val > val):
+            if(n.left is None):
+                return Exception
+            else:
+                return self.search(n.left, val)
+        elif(n.val < val):
+            if(n.right is None):
+                return Exception
+            else:
+                return self.search(n.right, val)
     
     def expand(self, n):
         n.left = Node(None)
@@ -114,12 +148,22 @@ class RBT:
         y.right = x
         x.parent = y
 
+    def minimum(self, x):
+        if(x.left.val is None):
+            return x
+        else:
+            return self.minimum(x.left)
+
     def print(self,tree,level):
+        if(level > self.bh):
+            self.bh = level
         if tree.right is not None:
             self.print(tree.right,level + 1)
         for i in range(level):
             print('   ', end='')
         print(tree.val,' , ',tree.color)
+        if(tree.color is 'black'):
+            self.nb += 1
         if tree.left is not None:
             self.print(tree.left, level + 1)
 
@@ -140,26 +184,113 @@ class RBT:
 
                 else:
                     finish = True
+
+    def transplant(self, u, v):
+        if(u.parent is None):
+            self.root = v
+        elif(u is u.parent.left):
+            u.parent.left = v
+        else:
+            u.parent.right = v
+        if(v is not None):
+            v.parent = u.parent
+
+    def delete(self, z):
+        y = z
+        original = y.color
+        if(z.left.color is None):
+            x = z.right
+            self.transplant(z, z.right)
+        elif(z.right.color is None):
+            x = z.left
+            self.transplant(z, z.left)
+        else:
+            y = self.minimum(z.right)
+            original = y.color
+            x = y.right
+            if(y.parent is z):
+                x.parent = y
+            else:
+                self.transplant(y, y.right)
+                y.right = z.right
+                y.right.parent = y
+            self.transplant(z, y)
+            y.left = z.left
+            y.left.parent = y
+            y.color = z.color
+        if(original is 'black'):
+            self.delete_Fixup(x)
+        self.total -= 1
+
+    def delete_Fixup(self, x):
+        while(x is not self.root and x.color is 'black'):
+            if(x is x.parent.left):
+                w = x.parent.right
+                if(w.color is 'red'):
+                    w.color = 'black'
+                    x.parent.color = 'red'
+                    self.leftRotate(x.parent)
+                    w = x.parent.right
+                if(w.left.color is 'black' and w.right.color is 'black'):
+                    w.color = 'red'
+                    x = x.parent
+                else:
+                    if(w.right.color is'black'):
+                        w.left.color = 'black'
+                        w.color = 'red'
+                        self.rightRotate(w)
+                        w = x.parent.right
+                    w.color = x.parent.color
+                    x.parent.color = 'black'
+                    self.leftRotate(x.parent)
+                    x = self.root
+            else:
+                w = x.parent.left
+                if(w.color is 'red'):
+                    w.color = 'black'
+                    x.parent.color = 'red'
+                    self.rightRotate(x.parent)
+                    w = x.parent.left
+                if(w.right.color is 'black' and w.left.color is 'black'):
+                    w.color = 'red'
+                    x = x.parent
+                else:
+                    if(w.left.color is'black'):
+                        w.right.color = 'black'
+                        w.color = 'red'
+                        self.leftRotate(w)
+                        w = x.parent.left
+                    w.color = x.parent.color
+                    x.parent.color = 'black'
+                    self.rightRotate(x.parent)
+                    x = self.root
+            x.color = 'black'
     
 def main():
+
     rbt = RBT()
-    rbt.insert(rbt.root, Node(41))
+    error = 0
+    f = open("input.txt", 'r')
+    lines = f.readlines()
+    for line in lines:
+        line = atoi(line)
+        if(line > 0):
+            rbt.insert(rbt.root, Node(line))
+        elif(line < 0):
+            line = abs(line)
+            delete = rbt.search(rbt.root, line)
+            
+            if(delete is Exception or delete is None):
+                error += 1
+            else:
+                rbt.delete(delete)
+        else:
+            break
+    f.close()
+    print('----------------------------------')
     rbt.print(rbt.root, 0)
-    print("\n")
-    rbt.insert(rbt.root, Node(38))
-    rbt.print(rbt.root, 0)
-    print("\n")
-    rbt.insert(rbt.root, Node(31))
-    rbt.print(rbt.root, 0)
-    print("\n")
-    rbt.insert(rbt.root, Node(12))
-    rbt.print(rbt.root, 0)
-    print("\n")
-    rbt.insert(rbt.root, Node(19))
-    rbt.print(rbt.root, 0)
-    print("\n")
-    rbt.insert(rbt.root, Node(8))
-    rbt.print(rbt.root, 0)
-    #print(rbt.root.left.color)
-    
+    print('----------------------------------')
+    print(rbt.total)
+    print(rbt.nb)
+    print(rbt.bh / 2)
 main() 
